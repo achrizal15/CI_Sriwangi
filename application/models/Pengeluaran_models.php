fb5918b1560b0475380511c80dceeb1b83cb561c
achrizal15 <achrizal15@gmail.com>
2021-09-06 21:46:41 +0700

    asc

--- application/models/Pengeluaran_models.php
+++ application/models/Pengeluaran_models.php
@@ -3,7 +3,7 @@ class Pengeluaran_models extends CI_Model
 {
     public function getAll()
     {
-        $hasil =  $this->db->query("SELECT pengeluaran.* , product_harga_beli, biayaumum_saldo FROM pengeluaran LEFT JOIN product ON pengeluaran.product_id = product.product_id LEFT JOIN biayaumum ON pengeluaran.biayaumum_id = biayaumum.biayaumum_id")->result_array();
+        $hasil =  $this->db->query("SELECT pengeluaran.* , product_harga_beli, biayaumum_saldo FROM pengeluaran LEFT JOIN product ON pengeluaran.product_id = product.product_id LEFT JOIN biayaumum ON pengeluaran.biayaumum_id = biayaumum.biayaumum_id ORDER BY pengeluaran_id ASC")->result_array();
         return $hasil;
     }
 }
